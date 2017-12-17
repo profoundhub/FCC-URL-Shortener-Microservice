5442e327220990cef9197be3fbcf7101ed37513a
Daniel <github@profoundideation.com>
2017-12-17 02:25:03 -0500

    review index.js
    review index.js

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const server = require("express");
 
 const cors = require("cors");
 const body-parser = require("body-parser");
+const fs = require("fs");
+const path = require("path");
 
-let fs = require("fs");
-let path = require("path");
 const mongoose = require("mongoose"); // let mongo = require("mongodb").MongoClient;
 const port = process.env.PORT || 8080;
+const dbUri = "mongodb://shortenuri:HishorturlsDB#users#datax:@ds159856.mlab.com:59856/shorturls";
+app.use(bodyParser,json());
+app.use('cors');
 
-const dbUri = "mongodb://<shortenuri:HishorturlsDB#users#datax:@ds159856.mlab.com:59856/shorturls";
-
+app.use('/',express.static('public'));
 
 app.use('/',express.static('public'));
 
